8b5be63628c287fbe007a87556882ee2d67d8883
Alexey Bashtanov <alexey.bashtanov@redpanda.com>
2025-03-17 11:47:18 +0000

    hashing/murmur: refactor x86_32 murmur3
    Separate block, tail and final stages to reuse them in later when
    implementing for non-continuous input (iobuf)

--- src/v/hashing/murmur.cc
+++ src/v/hashing/murmur.cc
@@ -57,40 +57,25 @@ getblock64(const uint64_t* p, int i) {
 
 } // namespace internal
 
-uint32_t murmurhash3_x86_32(const void* key, std::size_t len, uint32_t seed) {
-    const uint8_t* data = (const uint8_t*)key;
-    const int nblocks = len / 4;
+namespace {
+namespace x86_32 {
+const uint32_t c1 = 0xcc9e2d51;
+const uint32_t c2 = 0x1b873593;
 
-    uint32_t h1 = seed;
+void consume_block(uint32_t& h1, uint32_t block) {
+    block *= c1;
+    block = internal::rotl32(block, 15);
+    block *= c2;
 
-    const uint32_t c1 = 0xcc9e2d51;
-    const uint32_t c2 = 0x1b873593;
+    h1 ^= block;
+    h1 = internal::rotl32(h1, 13);
+    h1 = h1 * 5 + 0xe6546b64;
+}
 
-    //----------
-    // body
-
-    const uint32_t* blocks = (const uint32_t*)(data + nblocks * 4);
-
-    for (int i = -nblocks; i; i++) {
-        uint32_t k1 = internal::getblock32(blocks, i);
-
-        k1 *= c1;
-        k1 = internal::rotl32(k1, 15);
-        k1 *= c2;
-
-        h1 ^= k1;
-        h1 = internal::rotl32(h1, 13);
-        h1 = h1 * 5 + 0xe6546b64;
-    }
-
-    //----------
-    // tail
-
-    const uint8_t* tail = (const uint8_t*)(data + nblocks * 4);
-
+void consume_tail(uint32_t& h1, const uint8_t* tail, int len) {
     uint32_t k1 = 0;
 
-    switch (len & 3) {
+    switch (len) {
     case 3:
         k1 ^= tail[2] << 16;
     case 2:
@@ -102,13 +87,35 @@ uint32_t murmurhash3_x86_32(const void* key, std::size_t len, uint32_t seed) {
         k1 *= c2;
         h1 ^= k1;
     };
+}
 
-    //----------
-    // finalization
-
+void finalize(uint32_t& h1, int len) {
     h1 ^= len;
-
     h1 = internal::fmix32(h1);
+}
+} // namespace x86_32
+} // namespace
+
+uint32_t murmurhash3_x86_32(const void* key, std::size_t len, uint32_t seed) {
+    const uint8_t* data = (const uint8_t*)key;
+    const int nblocks = len / 4;
+
+    uint32_t h1 = seed;
+
+    auto blocks = reinterpret_cast<const uint32_t*>(data + nblocks * 4);
+
+    for (int i = -nblocks; i; i++) {
+        uint32_t k1 = internal::getblock32(blocks, i);
+        x86_32::consume_block(h1, k1);
+    }
+
+    const uint8_t* tail = (const uint8_t*)(data + nblocks * 4);
+    x86_32::consume_tail(h1, tail, len & 3);
+
+    x86_32::finalize(h1, len);
+
+    return h1;
+}
 
     return h1;
 }
