8a6fb396f68fb8743c9fa5eb27915f594d1ad1b5
Nicolae Vartolomei <nv@redpanda.com>
2025-08-11 18:25:33 +0100

    iceberg: remove now dead code

--- src/v/datalake/catalog_schema_manager.cc
+++ src/v/datalake/catalog_schema_manager.cc
@@ -149,8 +149,7 @@ simple_schema_manager::ensure_table_schema(
 
 ss::future<checked<schema_manager::table_info, schema_manager::errc>>
 simple_schema_manager::get_table_info(
-  const iceberg::table_identifier& table_id,
-  std::optional<std::reference_wrapper<iceberg::struct_type>>) {
+  const iceberg::table_identifier& table_id) {
     auto it = table_info_by_id.find(table_id);
     if (it == table_info_by_id.end()) {
         co_return errc::failed;
@@ -267,8 +266,7 @@ catalog_schema_manager::ensure_table_schema(
 
 ss::future<checked<schema_manager::table_info, schema_manager::errc>>
 catalog_schema_manager::get_table_info(
-  const iceberg::table_identifier& table_id,
-  std::optional<std::reference_wrapper<iceberg::struct_type>>) {
+  const iceberg::table_identifier& table_id) {
     auto gh = maybe_gate();
     if (gh.has_error()) {
         co_return gh.error();
--- src/v/datalake/catalog_schema_manager.h
+++ src/v/datalake/catalog_schema_manager.h
@@ -47,11 +47,8 @@ public:
         bool fill_registered_ids(iceberg::struct_type&);
     };
 
-    virtual ss::future<checked<table_info, errc>> get_table_info(
-      const iceberg::table_identifier&,
-      std::optional<std::reference_wrapper<iceberg::struct_type>> desired_type
-      = std::nullopt)
-      = 0;
+    virtual ss::future<checked<table_info, errc>>
+    get_table_info(const iceberg::table_identifier&) = 0;
 
     virtual ss::future<> stop() = 0;
     virtual ~schema_manager() = default;
@@ -70,10 +67,8 @@ public:
       const iceberg::struct_type& writer_struct_type,
       const iceberg::unresolved_partition_spec&) override;
 
-    ss::future<checked<table_info, schema_manager::errc>> get_table_info(
-      const iceberg::table_identifier&,
-      std::optional<std::reference_wrapper<iceberg::struct_type>> desired_type
-      = std::nullopt) override;
+    ss::future<checked<table_info, schema_manager::errc>>
+    get_table_info(const iceberg::table_identifier&) override;
     ss::future<> stop() final { return ss::now(); }
 
 private:
@@ -101,10 +96,8 @@ public:
       const iceberg::unresolved_partition_spec&) override;
 
     // Loads the table metadata for the given topic.
-    ss::future<checked<table_info, schema_manager::errc>> get_table_info(
-      const iceberg::table_identifier&,
-      std::optional<std::reference_wrapper<iceberg::struct_type>> desired_type
-      = std::nullopt) override;
+    ss::future<checked<table_info, schema_manager::errc>>
+    get_table_info(const iceberg::table_identifier&) override;
 
     // Stops the schema manager, waiting for any ongoing operations to
     // complete.
