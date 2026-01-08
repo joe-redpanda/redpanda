2f336f3b7de73d8db07b2519a8a765141f2b28ab
Brandon Allard <brandon@redpanda.com>
2026-01-08 00:05:09 -0500

    bytes: refactor iobuf::operator== to use iobuf::operator<=>
    This commit removes the existing implementation for `==` and adds one
    that uses `<=>` instead. This reduces the amount of code that needs to
    be checked for correctness. The overall performance of `==` doesn't seem
    to be negatively impacted by this change either.
    
    Before this commit:
    ```
    test                            iters            runtime     allocs      tasks       inst     cycles
    iobuf.eq_bench_small        400138000     2.02ns ± 0.08%      0.000      0.000      44.69        9.3
    iobuf.eq_bench_small_same   187854000     4.85ns ± 0.01%      0.000      0.000     132.69       24.2
    iobuf.eq_bench_medium         2012000     2.33ns ± 0.04%      0.000      0.000      44.69        9.9
    iobuf.eq_bench_medium_same     192000     4.74µs ± 0.03%      0.000      0.000   38931.70    25590.7
    iobuf.eq_bench_large           630000     2.23ns ± 0.04%      0.000      0.000      44.69        9.3
    iobuf.eq_bench_large_same       58000    15.96µs ± 0.13%      0.000      0.000  124434.22    85920.3
    ```
    
    After this commit:
    ```
    test                            iters            runtime     allocs      tasks       inst     cycles
    iobuf.eq_bench_small        387284000     2.10ns ± 0.01%      0.000      0.000      52.69        9.7
    iobuf.eq_bench_small_same   194184000     4.71ns ± 0.30%      0.000      0.000     123.69       23.6
    iobuf.eq_bench_medium         2012000     2.46ns ± 0.12%      0.000      0.000      52.69       10.6
    iobuf.eq_bench_medium_same     192000     4.75µs ± 0.06%      0.000      0.000   38923.70    25669.7
    iobuf.eq_bench_large           630000     2.33ns ± 0.24%      0.000      0.000      52.69        9.8
    iobuf.eq_bench_large_same       58000    15.88µs ± 0.01%      0.000      0.000  124408.72    85484.0
    ```
    
    (cherry picked from commit d3c2268f1446487f80bc04fb4928d57d4fdd23f2)

--- src/v/bytes/iobuf.cc
+++ src/v/bytes/iobuf.cc
@@ -107,47 +107,8 @@ bool iobuf::operator==(const iobuf& o) const {
     if (_size != o._size) {
         return false;
     }
-    if (!_frags.empty() && !o._frags.empty()) {
-        std::string_view lhs{_frags.front()};
-        std::string_view rhs{o._frags.front()};
-        constexpr static size_t max_byte_for_byte_cmp = 4;
-        auto n = std::min({lhs.size(), rhs.size(), max_byte_for_byte_cmp});
-        for (size_t i = 0; i < n; ++i) {
-            if (lhs[i] != rhs[i]) {
-                return false;
-            }
-        }
-    }
-    // We know these have the same amount of bytes in them, but they might
-    // be chunked differently.
-    auto o_it = o.cbegin();
-    auto other_next_view = [&o, &o_it] -> std::string_view {
-        while (o_it != o.cend() && o_it->is_empty()) {
-            ++o_it;
-        }
-        if (o_it == o.cend()) {
-            return {};
-        }
-        std::string_view s{o_it->get(), o_it->size()};
-        ++o_it;
-        return s;
-    };
-    std::string_view rhs = other_next_view();
-    for (const auto& frag : *this) {
-        std::string_view lhs{frag.get(), frag.size()};
-        while (!lhs.empty()) {
-            auto n = std::min(lhs.size(), rhs.size());
-            if (lhs.substr(0, n) != rhs.substr(0, n)) {
-                return false;
-            }
-            lhs.remove_prefix(n);
-            rhs.remove_prefix(n);
-            if (rhs.empty()) {
-                rhs = other_next_view();
-            }
-        }
-    }
-    return true;
+
+    return (*this <=> o) == std::strong_ordering::equal;
 }
 
 bool iobuf::operator<(const iobuf& o) const {
